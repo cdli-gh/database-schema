c6bcd7947be2bcc71db7066484a6aece5047600e
sengarsameer <sengar.sameer@gmail.com>
2018-07-21 17:17:27 +0530

    search script for cakePHP

---
+++ search/connectionstring.php
@@ -0,0 +1,14 @@
+<?php
+    class Connection {
+        public $default = array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
+            'host' => 'yourhost',
+            'login' => 'yourusername',
+            'password' => 'yourpassword',
+            'database' => 'search',
+            'prefix' => '',
+            //'encoding' => 'utf8',
+        );
+    }
+?>
---
+++ search/index.php
@@ -0,0 +1,10 @@
+<?php
+    if(isset($_POST['submit'])){
+
+    }
+    else { 
+        echo "<p>Enter a search query</p>";
+    }
+
+
+?>
